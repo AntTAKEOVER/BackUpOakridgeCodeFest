ea9404b3698b0aaa25e72f29622e69e5dea0ba1f
AntTAKEOVER <vedantpatwari01@gmail.com>
2018-01-20 15:09:48 +0530

    Finished the Binarizer
    Using the otsu threshold

--- OakJ/src/com/VedantPatwari/binarizer/Binarizer.java
+++ OakJ/src/com/VedantPatwari/binarizer/Binarizer.java
@@ -15,7 +15,11 @@ public class Binarizer {
         String output_f = args[0]+"_bin";
         original = ImageIO.read(original_f);
         grayscale = toGray(original);
+        binarized = binarize(grayscale);
+
+        writeImage(output_f);
     }
+
 
     private static BufferedImage toGray(BufferedImage original){
         int alpha, red, green, blue;
@@ -50,6 +54,95 @@ public class Binarizer {
         newPixel += blue;
 
         return newPixel;
+    }
+
+    private static BufferedImage binarize(BufferedImage original){
+        int red;
+        int newPixel;
+
+        int threshold = otsuThresh(original);
+
+        BufferedImage binarized = new BufferedImage(original.getWidth(), original.getHeight(), original.getType());
+
+        for(int i = 0; i < original.getWidth(); i++){
+            for(int j = 0; j<original.getHeight(); j++){
+                red  = new Color(original.getRGB(i, j)).getRed();
+                int alpha = new Color(original.getRGB(i, j)).getAlpha();
+                if(red > threshold){
+                    newPixel = 255;
+                }else{
+                    newPixel = 0;
+                }
+                newPixel = colorToRGB(alpha, newPixel, newPixel, newPixel);
+                binarized.setRGB(i, j, newPixel);
+            }
+        }
+
+        return binarized;
+    }
+
+    public static int otsuThresh(BufferedImage original){
+        int[] histogram = imageHistogram(original);
+        int total = original.getHeight() * original.getWidth();
+
+        float sum = 0;
+        for(int i = 0; i < 256; i++){
+            sum += i * histogram[i];
+        }
+
+        float sumB = 0;
+        int wB = 0;
+        int wF = 0;
+
+        float maxT = 0;
+        int threshold = 0;
+
+        for(int i = 0; i < 256; i++){
+            wB += histogram[i];
+            if(wB == 0){
+                continue;
+            }
+
+            wF = total - wB;
+
+            if(wF == 0){
+                break
+            }
+
+            sumB += (float) (i * histogram[i]);
+            float mB = sumB / wB;
+            float mF = (sum - sumB) / wF;
+
+            float varBetween = (float) wB * (float) * (mB - mF) * (mB - mF);
+
+            if(varBetween > maxT){
+                maxT = varBetween;
+                threshold = i;
+            }
+        }
+
+        return threshold;
+    }
+
+    public static int[] imageHistogram(BufferedImage input){
+        int[] histogram = new int[256];
+
+        for(int i = 0; i < histogram.length; i ++){
+            histogram[i] = 0;
+        }
+
+        for(int i = 0; i < input.getWidth(); i++){
+            for(int j = 0; j < input.getHeight(); j++){
+                int red = new Color(input.getRGB(i, j).getRed());
+                histogram[red]++;
+            }
+        }
+        return histogram;
+    }
+
+    private static void writeImage(String output) throws IOException{
+        File file = new File(output + ".jpg");
+        ImageIO.write(binarized, "jpg", file);
     }
 
 
